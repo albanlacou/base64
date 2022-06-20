2660e595fbc166d91fd1f0adc15d7c6a6a598d2f
Loic-Gouriou <loicgouriou6@gmail.com>
2022-06-20 15:06:11 +0200

    Fonction fill to octet

--- Loic.py
+++ Loic.py
@@ -3,3 +3,14 @@ def tab_to_string(tab_string):
     for i in tab_string :
         string = string + i
     return string
+
+def fill_to_octet(string):
+    nombre_tours = 0
+    for i in string :
+        nombre_tours = nombre_tours + 1
+        print(nombre_tours)
+    if nombre_tours < 8 :
+        for i in string :
+            string = string + "="
+            print(string)
+        return string
