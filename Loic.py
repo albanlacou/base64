486408d5e1e14b11eaa546a073997c2a11de4a94
Loic-Gouriou <loicgouriou6@gmail.com>
2022-06-20 15:31:27 +0200

    Fonction fill to octet, et mise en place de la docstring.

--- Loic.py
+++ Loic.py
@@ -1,16 +1,31 @@
 def tab_to_string(tab_string):
+    """
+    return : string
+    Rempli une string depuis un tableau
+    """
     string = ""
     for i in tab_string :
         string = string + i
     return string
 
+
 def fill_to_octet(string):
+    """
+    return : string
+    Ajoute le nombre d'egal pour que le nombres de caractères soit un multiple de 8
+    """
     nombre_tours = 0
     for i in string :
         nombre_tours = nombre_tours + 1
         print(nombre_tours)
-    if nombre_tours % 8 == 0:
-        for i in string :
+    if nombre_tours % 8 != 0 :
+        while nombre_tours % 8 != 0:
+            nombre_tours = nombre_tours + 1
             string = string + "="
-            print(string)
-        return string
+            print (string)
+    return string
+
+
+
+
+
